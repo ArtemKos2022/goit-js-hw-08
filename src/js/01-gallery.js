962cb6a96583fc953ed369cb8549ecf01556602f
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2022-12-22 20:26:03 +0200

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Change code below this line
 
-console.log(SimpleLightbox);
+// console.log(SimpleLightbox);
 
-// const galleryContainer = document.querySelector(".gallery")
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+    })
+    .join('');
+}
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
