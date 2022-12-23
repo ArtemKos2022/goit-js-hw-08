1ed829b05d058f5dc6bb3bd04791000c95514820
ArtemKos2022 <110256138+ArtemKos2022@users.noreply.github.com>
2022-12-23 19:49:05 +0200

    02-vidoe

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,16 @@
 import Player from '@vimeo/player';
-console.log(Player);
+// console.log(Player);
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const onCurrentTime = function (event) {
+  localStorage.setItem(CURRENT_TIME_KEY, event.seconds);
+};
+const savedTime = localStorage.getItem(CURRENT_TIME_KEY);
+player.setCurrentTime(savedTime).catch(function (error) {
+  console.log(error.message);
+});
+player.on('timeupdate', throttle(onCurrentTime, 1000));
